03386c1594a8880be4042a93eabf0acb8d8cc347
Your Name <fehmida1899@gmail.com>
2023-09-09 14:35:13 +0530

    Problem2

--- Problem2/src/com/goldilocks/CSV_ReadOperation.java
+++ Problem2/src/com/goldilocks/CSV_ReadOperation.java
@@ -5,41 +5,48 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
 //input output steam
 public class CSV_ReadOperation {
-	
+
 	public static void main(String[] args) {
 		String inputFile = "input.csv";
-        String outputFile = "output.csv";
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-           
-            String line;
-            
-            double sum = 0.0;
-            while ((line = reader.readLine()) != null) {
-                String[] val = line.split(",");
-                if (val.length >= 3) {
-                    double num1 = Double.parseDouble(val[0]);
-                    double num2 = Double.parseDouble(val[1]);
-                    double num3 = Double.parseDouble(val[2]);
-                    sum += num1 + num2 + num3;
+		String outputFile = "output.csv";
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
 
-                    writer.write(line + "," + (num1 + num2 + num3));
-                    writer.newLine();
-                }
-            }
+			String header = reader.readLine();
+			String line;
 
-            reader.close();
-            writer.close();
+			while ((line = reader.readLine()) != null) {
 
-            System.out.println("Output written to the " + outputFile +" file Successfully!!!");
-            System.out.println();
-            System.out.println("Go and check "+ outputFile + "file");
-            
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+				String[] val = line.split(",");
+				int sum = 0;
+				for (int i = 0; i < val.length; i++) {
+					
+					int num = Integer.parseInt(val[i].trim());
+					sum += num;
+				}
+				
+                if(null != header && !header.isEmpty()) {
+    				writer.write(header + "," + "Sum");
+    				writer.newLine();
+    				header = "";
+                }  
+				writer.write(line + "," + sum);
+				writer.newLine();
+			}
+
+			reader.close();
+			writer.close();
+
+			System.out.println("Output written to the " + outputFile + " file Successfully!!!");
+			System.out.println();
+			System.out.println("Go and check " + outputFile + "file");
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
